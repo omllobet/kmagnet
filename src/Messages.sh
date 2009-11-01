f1669f97330fcfc910b5549f1f31a582b53f859b
martlnl <omllobet@gmail.com>
2009-11-01 23:39:30 +0100

    added translations

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc` >> rc.cpp
-$XGETTEXT *.cpp -o $podir/kmagnet.pot
-rm -f *.cpp
+#$EXTRACTRC `find . -name \*.rc` >> rc.cpp
+#$XGETTEXT *.cpp -o $podir/kMagnet.pot
+extractrc `find . -name \*.rc` >> rc.cpp
+xgettext -ki18n -kI18N_NOOP  *.cpp -o ../po/kMagnet.pot
+#rm -f *.cpp
