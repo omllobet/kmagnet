38f1cc4cef49dd2d726e39422ffb298099b60955
martlnl <omllobet@gmail.com>
2009-11-01 23:42:19 +0100

    added translation files

---
+++ src/rc.cpp
@@ -0,0 +1,18 @@
+// i18n: file: kMagnetui.rc:5
+// i18n: ectx: Menu (game_play)
+i18n("&Play puzzle");
+// i18n: file: kMagnetui.rc:17
+// i18n: ectx: ToolBar (mainToolBar)
+i18n("Main Toolbar");
+// i18n: file: kMagnetui.rc:19
+// i18n: ectx: Menu (game_play_p)
+i18n("Puzzle");
+// i18n: file: kMagnetui.rc:5
+// i18n: ectx: Menu (game_play)
+i18n("&Play puzzle");
+// i18n: file: kMagnetui.rc:17
+// i18n: ectx: ToolBar (mainToolBar)
+i18n("Main Toolbar");
+// i18n: file: kMagnetui.rc:19
+// i18n: ectx: Menu (game_play_p)
+i18n("Puzzle");
