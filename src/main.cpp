fcf3f191d8a6e1bff2f4dc38bb9581283a01a0f5
“Oscar <omllobet@gmail.com>
2010-02-11 14:14:27 +0100

    removed some files

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@
 static const char description[] =
     I18N_NOOP("A simple puzzle-like game");
 
-static const char version[] = "0.06";
+static const char version[] = "0.07";
 
 uint Global::itemSize=20;//size of the cells
 
