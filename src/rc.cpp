f8521278d794030fd70d58820a3fdc3908ade14c
“Oscar <omllobet@gmail.com>
2010-02-15 11:02:37 +0100

    updated transaltio template file

--- src/rc.cpp
+++ src/rc.cpp
@@ -28,3 +28,9 @@ i18n("&Play puzzle");
 // i18n: file: kMagnetui.rc:17
 // i18n: ectx: ToolBar (mainToolBar)
 i18n("Main Toolbar");
+// i18n: file: kMagnetui.rc:5
+// i18n: ectx: Menu (game_play)
+i18n("&Play puzzle");
+// i18n: file: kMagnetui.rc:18
+// i18n: ectx: ToolBar (mainToolBar)
+i18n("Main Toolbar");
